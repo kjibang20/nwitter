d11b36ffcbfea8286867153f7b250e670fb4aafa
kjibang20 <kjibang20@gmail.com>
2026-02-20 09:58:43 +0900

    1.0 Initialization

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import AppRouter from "./Router";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+
+export default App;
