756f95a38d36db1b69f4dc988962c9797f370aa1
kjibang20 <kjibang20@gmail.com>
2026-03-15 15:38:39 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ7-UhDsCqRHcuwJA8EvZ1PP_6poPQjnQ",
+  authDomain: "nwitt-7d1a1.firebaseapp.com",
+  projectId: "nwitt-7d1a1",
+  storageBucket: "nwitt-7d1a1.firebasestorage.app",
+  messagingSenderId: "638119022293",
+  appId: "1:638119022293:web:f9d2b23acb92d216045c5c",
+  measurementId: "G-Y8BBQRQRF3",
+};
+
+export default firebase.initializeApp(firebaseConfig);
